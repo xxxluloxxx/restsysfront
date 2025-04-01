8c416cf007967570be57c3186b3d7508d10ab927
Paul Arias <paul.arias@bayteq.com>
2025-04-01 15:30:54 -0500

    Despliegue

--- vite.config.mjs
+++ vite.config.mjs
@@ -30,5 +30,9 @@ export default defineConfig({
                 manualChunks: undefined
             }
         }
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 3000
     }
 });
